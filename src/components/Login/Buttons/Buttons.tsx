96260748a3c905db57b735a27b415ba76ee7a1ed
LuciLua <luci.lua81@gmail.com>
2022-12-23 17:51:36 -0300

    :star2: BEM => c-btn component

--- src/components/Login/Buttons/Buttons.tsx
+++ src/components/Login/Buttons/Buttons.tsx
@@ -3,13 +3,13 @@ import styles from "./Buttons.module.scss"
 
 function Buttons() {
     return (
-        <div className={styles.buttons}>
-            <button>
+        <div className={styles.c_buttons}>
+            <button className={styles.c_btn}>
                 <Link href={'/logged'}>
                     Login
                 </Link>
             </button>
-            <div className={styles.create}>
+            <div className={styles.c_btn_create}>
                 <span>
                     <Link href={'#'}>
                         Crie uma conta aqui
